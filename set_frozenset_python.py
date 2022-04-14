765bcbb57b5a5f3f777458e46e6155e26f0e613f
yevanalfi <81598283+yevanalfi@users.noreply.github.com>
2022-04-14 23:48:54 +0700

    Add files via upload

---
+++ set_frozenset_python.py
@@ -0,0 +1,6 @@
+contoh_list=['Dewi','Budi','Cici','Linda','Cici']
+print(contoh_list)
+data_set={'Dewi','Budi','Cici','Linda','Cici'}
+print(data_set)
+frozen_set=({'Dewi','Budi','Cici','Linda','Cici'})
+print(frozen_set)
